e7603652e8cb0dd5d9442e91bf74bd55db87a4e4
swifu69 <xlrgameryt04d@gmail.com>
2022-03-24 18:28:32 +0000

    Fixed some visual effects. Now its possible to log in

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoDb = require("mongoose");
 const userSchema = new mongoDb.Schema({
 	Email: { type: String, required: true, unique: true },
 	Password: { type: String, required: true },
-	Salt: { type: String, required: true },
-	Role: { type: String, enum: ["user","admin","gay"], default: "user"}
+	//Salt: { type: String, required: true },
+	Role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
 const User = mongoDb.model("User", userSchema);
--- routes/sign.js
+++ routes/sign.js
@@ -11,18 +11,16 @@ router.post("/sign", async (req, res) => {
 		const body = req.body;
 
 		if (!(body.Email && body.Password)) {
-			return res.status(400).send({ error: "Data not formatted properly" });
+			req.flash("errorMessage", "Make sure you fill both input fields!");
+			res.redirect("/sign");
+		} else {
+			const user = new User(body);
+			const salt = await bcrypt.genSalt(10);
+			user.Salt = salt;
+			user.Password = await bcrypt.hash(body.Password, salt);
+			await user.save();
+			res.redirect("/login");
 		}
-
-		// creating a new mongoose doc from user data
-		const user = new User(body);
-		// generate salt to hash password
-		const salt = await bcrypt.genSalt(10);
-		// now we set user password to hashed password
-		user.Salt = salt;
-		user.Password = await bcrypt.hash(body.Password, salt);
-		await user.save();
-		res.redirect("/login");
 	} catch (err) {
 		res.status(500).json(err);
 		console.log(err);
